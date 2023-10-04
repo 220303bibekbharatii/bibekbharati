4c3a51bc819653f427e17de746e891bff0d747cc
220303_bibekbharati <127607551+220303bibekbharatii@users.noreply.github.com>
2023-10-04 07:44:27 +0545

    Add files via upload
    loop

---
+++ loop.py
@@ -0,0 +1,13 @@
+for i in range(1, 101):
+    print(i)
+
+# WAP to print 50 to 10.
+for i in range(50, 9, -1):
+    print(i)
+# WAP to print even number from 10 to 41
+for i in range(10, 42, +2):
+    print(i)
+# wap to print odd numbers from 100 to 50 
+for i in range(99, 49, -2):
+    print(i, end= '')
+ 
