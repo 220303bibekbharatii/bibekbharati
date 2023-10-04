b5005790f188718af74ac7efb04476c831c1dd80
220303_bibekbharati <127607551+220303bibekbharatii@users.noreply.github.com>
2023-10-04 08:13:23 +0545

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,29 @@
+# menu driven programs
+#1. Addition
+#2. Subtraction
+#3. Multiplication
+#4. Division
+#5. Exit 
+
+ 
+#  Write a menu driven program for following case
+#1. hello
+#2. hi
+#3. Bye 
+#4. Welcome
+#5. exit
+
+x=int(input("Enter a choice from 1 to 5"))
+if (x==1):
+    print("hello")
+elif(x==2):
+    print("hi")
+elif(x==3):
+    print("bye")
+elif(x==4):
+    print("Welcome")
+elif(x==5):
+    print("exit")
+else:
+    print("out of range")
+
