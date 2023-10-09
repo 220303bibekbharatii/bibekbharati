244f7191af81c3cca504ad7136274322706022b0
220303_bibekbharati <127607551+220303bibekbharatii@users.noreply.github.com>
2023-10-09 07:43:02 +0545

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,29 +1,71 @@
-# menu driven programs
-#1. Addition
-#2. Subtraction
-#3. Multiplication
-#4. Division
-#5. Exit 
 
- 
-#  Write a menu driven program for following case
-#1. hello
-#2. hi
-#3. Bye 
-#4. Welcome
-#5. exit
 
-x=int(input("Enter a choice from 1 to 5"))
-if (x==1):
-    print("hello")
-elif(x==2):
-    print("hi")
-elif(x==3):
-    print("bye")
-elif(x==4):
-    print("Welcome")
-elif(x==5):
-    print("exit")
-else:
-    print("out of range")
+# write a menu driven program using function for the following
+ # 1. WAP to calculate smaller between two numbers 
+
+
+  
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+
+# if num1 < num2:
+#             print(num1, "is the smallest number.")
+# elif num2 < num1:
+#         print(num2, "is the smallest number.")
+# else:
+#     print("Both numbers are equal.")
+
+
+# exit()
+
+
+
+ # 2. WAP to calculate greatest among any three numbers
+
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+# num3 = float(input("Enter the third number: "))
+
+# if num1 > num2 :
+#             print(num1, "is the greatest number.")
+# elif num2 >  num3:
+#         print(num2, "is the greatest number.")
+# elif num3 >  num1:
+#         print(num3, "is the greatest")        
+
+
+# exit()
+
+
+ # 3. WAP to print the middle numbers among three numbers
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+# num3 = float(input("Enter the third number: "))
+
+# if num1 <= num2 <= num3 or num3 <= num2 <= num1:
+#     middle_num = num2
+# elif num2 <= num1 <= num3 or num3 <= num1 <= num2:
+#     middle_num = num1
+# else:
+#     middle_num = num3   
+# print("The middle number among", num1, ",", num2, "and", num3, "is:", middle_num)       
+
+# exit()
+ # 4. WAP to print the following series.
+     # 5 to 10 15 20.... on upto tenth terms
+# i=5
+# while(i<=50):
+#     print(i)
+#     i=i+5
+
+ #6. WAP to print the odd numbers from 100 to 20
+
+ # 7. exit 
+ # and also ask user to comtinue the menu choice
+ #8. WAP to print the Fibonacci series upto 10th term
+ # WAP to check wether the entered number is Armstrong or not.
+
+      
+
+
 
