680ad850b8f75cb29857b6553ee1a1c221fa60d2
220303_bibekbharati <127607551+220303bibekbharatii@users.noreply.github.com>
2023-12-01 08:50:52 +0545

    Add files via upload

---
+++ list.py
@@ -0,0 +1,46 @@
+# batch=["ethical"]
+# sec= ("A","B")
+# print(type(batch))
+# print(type(sec))
+
+# batch= ["ethical","computing","computer science"]
+# sec= ("A","B","C")
+# print(type(batch))
+# print(type(sec))
+# Existing tuple
+
+# original_tuple = (1, 2, 3, 4)
+# new_element = 5
+
+# new_tuple = original_tuple + (new_element,)
+
+# print("Original Tuple:", original_tuple)
+# print("New Tuple with Added Element:", new_tuple)
+
+# # 2. Perform following functions in collecttions. 
+# printing the collection
+# my_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+
+# print("Original List:", my_list)
+# print the collection using constructor
+# printing range selection along with negative indexing
+# my_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+# print("Range selection (index 2 to 7):", my_list[2:8])
+# print("Negative indexing (last 3 elements):", my_list[-3:])
+#     length checking
+# my_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+# print("Length of the list:", len(my_list))
+
+#     type checking
+my_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+print("Type of collection:", type(my_list))
+
+#     counting same values
+#     insert
+#     delete
+#     sorting(ascending and desending)
+#     reverse
+#     extending and joining
+#     allow duplicate or not
+
+
